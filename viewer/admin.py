fe7174f4721a9c22b4ae3b4287becca7e4b8b856
lalachmoon <109298737+lalachmoon@users.noreply.github.com>
2024-12-08 16:04:25 +0200

    Added admin registration

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from viewer.models import City, Neighborhood, Street, Courier
+
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Neighborhood)
+admin.site.register(Street)
+admin.site.register(Courier)
